7d06e122bf0702a8a5e52ce97e912187149d8ee3
noorshalayel <noorshalayel@gmail.com>
2022-05-24 17:51:41 +0300

    update6

---
+++ app/src/main/java/com/example/alnour/UpdateSupplierActivity.java
@@ -0,0 +1,24 @@
+package com.example.alnour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class UpdateSupplierActivity extends AppCompatActivity {
+    public TextInputEditText name, phone, email , address;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_supplier);
+        init();
+    }
+    public void init(){
+        name = findViewById(R.id.name);
+        phone = findViewById(R.id.phonenumber);
+        email = findViewById(R.id.email);
+        address = findViewById(R.id.address);
+    }
+}
