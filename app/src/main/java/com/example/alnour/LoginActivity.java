4bc301c8f854aebb60bfb2d03808c5bfd3a4218d
Your Name <you@example.com>
2022-05-25 21:40:31 +0300

    auth login

--- app/src/main/java/com/example/alnour/LoginActivity.java
+++ app/src/main/java/com/example/alnour/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.alnour;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,14 +13,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
     private Button login;
-
     private TextView forgot;
+    FirebaseAuth firAuth ;
 
-    private TextInputEditText email, password;
+    private TextInputEditText et_email, et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,24 +36,46 @@ public class LoginActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
 
-
         setContentView(R.layout.activity_login);
 
-        login = findViewById(R.id.login);
 
-        forgot = findViewById(R.id.forgot);
+        inti();
+        lisner();
 
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
+    }
 
+    private void auth() {
+        String email =  et_email.getText().toString();
+        String password =  et_password.getText().toString();
+
+        if (email.isEmpty()){
+            Toast.makeText(this , "Email Empty !!" , Toast.LENGTH_SHORT).show();
+        }else if (password.isEmpty()){
+            Toast.makeText(this , "Password Empty !!" , Toast.LENGTH_SHORT).show();
+        }else{
+            firAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        Toast.makeText(LoginActivity.this, "email or password wrong", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void lisner() {
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(LoginActivity.this, "Login", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                auth();
             }
         });
 
@@ -57,5 +85,13 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(LoginActivity.this, "Forgot", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void inti() {
+        firAuth = FirebaseAuth.getInstance();
+        login = findViewById(R.id.login);
+        forgot = findViewById(R.id.forgot);
+        et_email = findViewById(R.id.email);
+        et_password = findViewById(R.id.password);
     }
 }
