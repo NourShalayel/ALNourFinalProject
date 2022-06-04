5d3fd9a1a7f968941df277b9cac325160f54758a
noorshalayel <noorshalayel@gmail.com>
2022-06-04 17:33:17 +0300

    search

--- app/src/main/java/com/example/alnour/MainActivity.java
+++ app/src/main/java/com/example/alnour/MainActivity.java
@@ -14,6 +14,7 @@ import android.app.DownloadManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -29,6 +30,8 @@ import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.example.alnour.classes.Person;
+import com.example.alnour.classes.Product;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -48,41 +51,59 @@ import com.google.gson.Gson;
 
 import java.net.CookieManager;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+     SearchView searchView;
+    ArrayList<Product> pro_list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        searchView = (SearchView) findViewById(R.id.serach_view);
+
 
 
 
     }
 
+//    private void fileList(String newText) {
+//        List<Product> filterList = new ArrayList<>();
+//        for(Product pro : pro_list){
+//            if(pro.getName().toLowerCase().contains(newText.toLowerCase())){
+//                filterList.add(pro);
+//            }
+//        }
+//        if(filterList.isEmpty()){
+//            Toast.makeText(this , "No Data Found" , Toast.LENGTH_SHORT).show();
+//        }else{
+//
+//        }
+//    }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        SearchView sv = (SearchView) item.getActionView();
-        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        getMenuInflater().inflate(R.menu.main_menu, menu);
+//        MenuItem item = menu.findItem(R.id.action_search);
+//        SearchView sv = (SearchView) item.getActionView();
+//        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                return false;
+//            }
+//        });
+//
+//        return super.onCreateOptionsMenu(menu);
+//    }
 
 
 }
--- app/src/main/java/com/example/alnour/product/ProductFragment.java
+++ app/src/main/java/com/example/alnour/product/ProductFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -29,6 +30,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProductFragment extends Fragment {
 
@@ -38,6 +40,7 @@ public class ProductFragment extends Fragment {
     FloatingActionButton addProduct ;
     ArrayList<Person> sup_list = new ArrayList<>();
     ArrayList<Category> cat_list = new ArrayList<>();
+    MainActivity mainActivity ;
 
     public ProductFragment() {
         // Required empty public constructor
@@ -62,7 +65,19 @@ public class ProductFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        mainActivity.searchView.clearFocus();
+        mainActivity.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                fileList(newText);
+                return true;
+            }
+        });
 
         return  v ;
     }
@@ -157,6 +172,20 @@ public class ProductFragment extends Fragment {
                 }
             }
         });
+    }
+
+    private void fileList(String newText) {
+        List<Product> filterList = new ArrayList<>();
+        for(Product pro : pro_list){
+            if(pro.getName().toLowerCase().contains(newText.toLowerCase())){
+                filterList.add(pro);
+            }
+        }
+        if(filterList.isEmpty()){
+            Toast.makeText(getContext() , "No Data Found" , Toast.LENGTH_SHORT).show();
+        }else{
+
+        }
     }
 
 }
